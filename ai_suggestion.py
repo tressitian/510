192248be727bef404e807525d31db6eef5c43fe6
kw-mb <kw@galleria>
2025-02-22 13:51:17 -0800

    -

---
+++ ai_suggestion.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_ai_suggestion():
+    st.header("AI Suggestion and Try-On")
+    st.write("This is where the AI suggestions and try-on feature will be implemented.")
---
+++ clothes_suggestion.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_clothes_suggestion():
+    st.header("New Clothes Suggestions")
+    st.write("This is where new clothes suggestions and item links will be displayed.")
